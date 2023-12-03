364d2ea344c5de21fe297bbd446839b866ce2b9b
George Borrelli <gpb8977@163.com>
2023-12-03 22:16:45 +0800

    remove html tag from stylesheet selector

--- src/modules/config.js
+++ src/modules/config.js
@@ -53,6 +53,9 @@ const config = {
   defaultDarkTextColor: DEFAULT_DARK_TEXTCOLOR,
   defaultDarkBgColor: DEFAULT_DARK_BGCOLOR,
 
+  darkModeClassEl: null,
+  appendStylesEl: null,
+
   // 设置配置
   set(type, opt, key) {
     const value = opt[key];
